964e63f6c67d582e7a5a37807953453877f63f7b
Colm Larkin <clarkin@gmail.com>
2013-08-25 23:27:57 +0100

    first screen of steps added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
-    chainIn(['.header h1', '.header h4', '.intro-title', '.intro-features', '.intro-button'], 0);
+
+    chainIn(['.header h1', '.header h4', '.intro-title', '.intro-features', '.intro-button']);
 
     $('#btnstart').click(function () {
-        $('.intro').hide(300);
-        //$('.progress-completed').show(300);
-        //$('.creation-1').show(300);
-        chainIn(['.progress-completed', '.creation-1'], 0);
+        $('.intro').fadeOut(300, function () {
+            $('.header').addClass('docked');
+            setTimeout(function () {
+                chainIn(['.header-right', '.progress-completed', '.step-race', '.next-step']);
+            }, 1000);
+        });
+
+    });
+
+    $('.step-race a').click(function () {
+        console.log('clicked');
+        chainIn(['.step-subrace']);
     });
 });
 
 var chainIn = function (toAnimate, ix) {
+    if (typeof ix === 'undefined') {
+        ix = 0;
+    }
+
     if (toAnimate[ix]) {
-        $(toAnimate[ix]).fadeIn(1500, function () { chainIn(toAnimate, ix + 1) });
+        if (typeof toAnimate[ix] === 'string') {
+            $(toAnimate[ix]).fadeIn(1500, function () { chainIn(toAnimate, ix + 1) });
+        } 
     }
 };
+
+
 
