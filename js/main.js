6d4eee4fa097869ecde70d9ace21005022788338
Colm Larkin <clarkin@gmail.com>
2013-08-29 23:08:46 +0100

    added textbox type question; made transitions even nicer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var ANIMATE_SPEED = 1500;
-var SPEEDYMODE = true;
+var SPEEDYMODE = false;
 if (SPEEDYMODE) {
     ANIMATE_SPEED = ANIMATE_SPEED / 10;
 }
@@ -19,16 +19,37 @@ $(document).ready(function () {
     var q;
 
     q = createQuestion('Choose your RACE');
-    q.addAnswerWithSub('Human', 'What kind of HUMAN?', ['Desert Nomad', 'Ice Barbarian', 'Empire Citizen']);
-    q.addAnswerWithSub('Elf', 'What kind of ELF?', ['Dark Drow', 'Sylvan Elf', 'Sea Elf', 'High Elf']);
-    q.addAnswerWithSub('Dwarf', 'What kind of DWARF?', ['Dark Duergar', 'Mountain Dwarf', 'Mechanical Dwarf']);
+    q.addAnswerWithSub('Human', 'What kind of HUMAN?', ['Desert Nomad', 'Ice Barbarian', 'Empire Citizen', 'Jungle Tribesman', 'Plainswalker']);
+    q.addAnswerWithSub('Elf', 'What kind of ELF?', ['Dark Drow', 'Sylvan Elf', 'Sea Elf', 'High Elf', 'Cosplay Elf']);
+    q.addAnswerWithSub('Dwarf', 'What kind of DWARF?', ['Dark Duergar', 'Mountain Dwarf', 'Mechanical Dwarf', 'Stereotypical Dwarf']);
+    q.addAnswerWithSub('Gnome', 'What kind of GNOME?', ['Jungle Gnome', 'Sea Gnome', 'Giant Gnome', 'Chaos Gnome', 'Garden Gnome']);
+    q.addAnswerWithSub('Halfling', 'What kind of HALFLING?', ['Hairfoot Halfling', 'Stout Halfling', 'Tallfellow Halfling', 'Half-Halfling']);
+    q.addAnswerWithSub('Half-Orc', 'What kind of HALF-ORC?', ['Hairy Half-Orc', 'Ugly Half-Orc', 'Smelly Half-Orc']);
+    q.addAnswerWithSub('Lycanthrope', 'What kind of LYCANTHROPE?', ['Werewolf', 'Werebear', 'Weretiger', 'Wereboar', 'Wererat', 'Wereshark']);
+    q.addAnswerWithSub('Golem', 'What kind of GOLEM?', ['Flesh Golem', 'Stone Golem', 'Iron Golem', 'Clay Golem', 'Brass Golem', 'Poop Golem']);
+    q.addAnswerWithSub('Demon', 'What kind of DEMON?', ['Succubus', 'Vrock', 'Quasit', 'Nalfeshnee', 'Dretch', 'Balor']);
     steps.push(q);
 
     q = createQuestion('Choose your SEX');
-    q.addAnswerWithSub('Human', 'What kind of HUMAN?', ['Desert Nomad', 'Ice Barbarian', 'Empire Citizen']);
-    q.addAnswerWithSub('Elf', 'What kind of ELF?', ['Dark Drow', 'Sylvan Elf', 'Sea Elf', 'High Elf']);
-    q.addAnswerWithSub('Dwarf', 'What kind of DWARF?', ['Dark Duergar', 'Mountain Dwarf', 'Mechanical Dwarf']);
+    q.addAnswersArray(['Male', 'Female', 'It\'s Complicated']);
     steps.push(q);
+
+    q = createQuestion('Choose your CLASS');
+    q.addAnswerWithSub('Fighter', 'What kind of FIGHTER?', ['Warrior', 'Paladin', 'Thug', 'Barbarian', 'Ranger', 'Monk', 'Cavalier', 'Blackguard']);
+    q.addAnswerWithSub('Magician', 'What kind of MAGICIAN?', ['Wizard', 'Illusionist', 'Necromancer', 'Abjurer', 'Sorcerer', 'Sourcerer', 'Emomancer', 'Mathemagician']);
+    q.addAnswerWithSub('Priest', 'What kind of PRIEST?', ['Cleric', 'Druid', 'Warpriest', 'Witch']);
+    q.addAnswerWithSub('Rogue', 'What kind of Rogue?', ['Thief', 'Assassin', 'Bard', 'Swashbuckler', 'Cat Burglar', 'Scout', 'Rouge']);
+    steps.push(q);
+
+    q = createQuestion('Choose your NAME');
+    q.type = 'textbox';
+    steps.push(q);
+
+    /*
+    q = createQuestion('Choose your CLASS');
+    q.addAnswerWithSub('Fighter', 'What kind of FIGHTER?', ['Desert', 'Ice', 'Empire', 'Jungle', 'Plainswalker']);
+    steps.push(q);
+    */
 
     totalSteps = steps.length;
 
@@ -46,10 +67,11 @@ $(document).ready(function () {
                 chainIn(['.header-right', '.progress-completed', '.next-step']);
                 setTimeout(function () {
                     initStep(currentStep);
-                }, ANIMATE_SPEED * 2);
+                }, ANIMATE_SPEED * 3);
             }, ANIMATE_SPEED);
         }, ANIMATE_SPEED_OUT * 3);
     });
+    //$('#btnstart').click();
 
     $('#give-up-button').click(function (e) {
         e.preventDefault();
@@ -116,22 +138,31 @@ $(document).ready(function () {
             initSubStep(thisAnswer.subQuestion);
         } else {
             //console.log("subquestion answered");
-            checkAnswer(thisAnswer);
-            
+            checkAnswer(thisAnswer);   
         }
+    });
 
+    $('.step-basic .form-holder input[type=text]').on('input change keydown', function () {
+        if ($(this).val().length > 0) {
+            checkAnswer({ answer: $(this).val() });
+        }
     });
 
 });
 
 var createQuestion = function (question) {
-    return { question: question, answers: [], addAnswersArray: addAnswersArray, addAnswerWithSub: addAnswerWithSub };
+    return { question: question, answers: [], type: 'multiple-choice', addAnswersArray: addAnswersArray, addAnswer: addAnswer, addAnswerWithSub: addAnswerWithSub };
 }
 
 var createAnswer = function (forQuestion, answer, subQuestion) {
     var thisAnswer = { forQuestion: forQuestion, answer: answer, subQuestion: subQuestion };
     forQuestion.answers.push(thisAnswer);
     return thisAnswer;
+}
+
+var addAnswer = function (answer) {
+    var q = this;
+    createAnswer(q, answer);
 }
 
 var addAnswerWithSub = function (answer, subQuestion, subQuestionAnswersArray) {
@@ -164,21 +195,26 @@ var checkAnswer = function (thisAnswer) {
 var initStep = function (stepNo) {
     if (stepNo < steps.length) {
         thisStep = steps[stepNo];
-        //console.log(thisStep);
+        console.log(thisStep);
 
         $('.progress-completed .section-number').text('Section ' + (stepNo + 1) + ' - ');
-        $('.progress-completed .percentage-complete').text('Complete ' + (percentageComplete * 100) + '%');
-        chainIn(['.progress-completed .text']);
+        $('.progress-completed .percentage-complete').text('Complete ' + (percentageComplete * 100).toFixed(2) + '%');
 
         resetAnswer();
         $('.step-basic').removeClass('docked');
         $('.step-basic h2').html(thisStep.question);
         $('.step-basic .btn-group').html("");
+        $('.step-basic .btn-group').hide();
         $.each(thisStep.answers, function (index, value) {
+            $('.step-basic .btn-group').show();
             $('.step-basic .btn-group').append('<a class="btn btn-custom">' + value.answer + '</a>');
         });
+        $('.step-basic .form-holder').hide();
+        if (thisStep.type === 'textbox') {
+            $('.step-basic .form-holder').show();
+        }
 
-        chainIn(['.step-basic']);
+        chainIn(['.progress-completed .text', '.step-basic']);
     } else {
         $('.progress-completed .section-number').text('');
         $('.progress-completed .percentage-complete').text('Complete ' + (percentageComplete * 100) + '%');
@@ -206,7 +242,7 @@ var initSubStep = function (question) {
 var showEndGame = function () {
     isFinished = true;
     chainOut(['.step-basic-sub', '.step-basic', '.next-step']);
-    $('.game-over h3 b').text((percentageComplete * 100) + '%');
+    $('.game-over h3 b').text((percentageComplete * 100).toFixed(2) + '%');
     chainIn(['.game-over']);
 }
 
@@ -241,11 +277,11 @@ var chainOut = function (toAnimate) {
 }
 
 var setTransition = function (element, animationSpeed) {
-    $(element).css('-webkit-transition', 'all ' + animationSpeed + 'ms ease');
-    $(element).css('-moz-transition', 'all ' + animationSpeed + 'ms ease');
-    $(element).css('-ms-transition', 'all ' + animationSpeed + 'ms ease');
-    $(element).css('-o-transition', 'all ' + animationSpeed + 'ms ease');
-    $(element).css('transition', 'all ' + animationSpeed + 'ms ease');
+    $(element).css('-webkit-transition', 'all ' + animationSpeed + 'ms ease, opacity ' + animationSpeed + 'ms ease-out ' + animationSpeed / 4 + 'ms');
+    $(element).css('-moz-transition', 'all ' + animationSpeed + 'ms ease, opacity ' + animationSpeed + 'ms ease-out ' + animationSpeed / 4 + 'ms');
+    $(element).css('-ms-transition', 'all ' + animationSpeed + 'ms ease, opacity ' + animationSpeed + 'ms ease-out ' + animationSpeed / 4 + 'ms');
+    $(element).css('-o-transition', 'all ' + animationSpeed + 'ms ease, opacity ' + animationSpeed + 'ms ease-out ' + animationSpeed / 4 + 'ms');
+    $(element).css('transition', 'all ' + animationSpeed + 'ms ease, opacity ' + animationSpeed + 'ms ease-out ' + animationSpeed / 4 + 'ms');
 }
 
 
